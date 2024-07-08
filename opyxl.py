a932f7609632831d3d0aff02b6f5e714b5fe1679
anselmobd <anselmo.blanco.dominguez@gmail.com>
2024-07-08 11:44:13 -0300

    criando arquivo de output

--- opyxl.py
+++ opyxl.py
@@ -1,4 +1,4 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook, styles
 from pprint import pprint
 
 
@@ -10,8 +10,9 @@ class EquiqXlsx:
     QUANT = 'Quantidade'
     TIPO = 'Tipo Ativo'
 
-    def __init__(self, filename):
+    def __init__(self, filename, destino):
         self.filename = filename
+        self.destino = destino
 
         self.origin_columns_idx = {
             self.DESCR: None,
@@ -132,10 +133,15 @@ class EquiqXlsx:
                     self.data_append_item(idx_row, row)
 
     def print(self):
-        pprint(self.destination)
+        # pprint(self.destination)
         print(sum(self.destination.values()), 'itens')
         pprint(self.tags)
         pprint(self.anos)
+
+    def create_xlsx(self):
+        self.dest_wb = Workbook()
+        self.dest_ws = self.dest_wb.active
+        self.bold_font = styles.Font(bold=True)
 
     def get_tags_anos(self):
         tags = set()
@@ -146,12 +152,70 @@ class EquiqXlsx:
         self.tags = sorted(list(tags))
         self.anos = sorted(list(anos))
 
+    def set_tags(self):
+        tags = ['Tag \ Ano'] + self.tags + ['Totais']
+        max_length = 0
+        for line, tag in enumerate(tags, start=1):
+            max_length = max(len(tag), max_length)
+            cell = self.dest_ws.cell(row=line, column=1, value=tag)
+            cell.font = self.bold_font
+            cell.alignment = styles.Alignment(horizontal='center')  
+        self.dest_ws.column_dimensions['A'].width = max_length + 2
+
+    def set_anos(self):
+        for coluna, ano in enumerate(self.anos + ['Totais'], start=2):
+            cell = self.dest_ws.cell(row=1, column=coluna, value=ano)
+            cell.font = self.bold_font
+            self.dest_ws.column_dimensions[cell.column_letter].width = 6
+            cell.alignment = styles.Alignment(horizontal='right')  
+        # a última coluna, de totais, redefina largura para 8
+        self.dest_ws.column_dimensions[cell.column_letter].width = 8
+
+    def set_valores(self):
+        for key, value in self.destination.items():
+            tag, ano = key
+            idx_tag = self.tags.index(tag) + 2
+            idx_ano = self.anos.index(ano) + 2
+            self.dest_ws.cell(row=idx_tag, column=idx_ano, value=value)
+
+    def tot_anos(self):
+        for coluna, _ in enumerate(self.anos, start=2):
+            linha_totais = len(self.tags) + 2
+            cell = self.dest_ws.cell(row=linha_totais, column=coluna)
+            cell.font = self.bold_font
+            letra = cell.column_letter
+            cell.value = f'=SUM({letra}2:{letra}{linha_totais-1}'
+
+    def tot_tags(self):
+        # soma inclusive a linha de totais por ano
+        for linha in range(2, len(self.tags)+3):
+            coluna_totais = len(self.anos) + 2
+            cell = self.dest_ws.cell(row=linha, column=coluna_totais)
+            cell_esquerda = self.dest_ws.cell(row=linha, column=coluna_totais-1)
+            letra_esquerda = cell_esquerda.column_letter
+            cell.value = f'=SUM(B{linha}:{letra_esquerda}{linha}'
+            cell.font = self.bold_font
+
+    def make_analise(self):
+        self.dest_ws['A1'] = 'Tag \ Ano'
+        self.set_tags()
+        self.set_anos()
+        self.set_valores()
+        self.tot_anos()
+        self.tot_tags()
+
+    def save(self):
+        self.get_tags_anos()
+        self.create_xlsx()
+        self.make_analise()
+        self.dest_wb.save(self.destino)
+
     def process(self):
         self.load()
         self.walk_through()
-        self.get_tags_anos()
-        self.print()
+        self.save()
+        # self.print()
 
 
 if __name__ == '__main__':
-    EquiqXlsx('equipamentos.xlsx').process()
+    EquiqXlsx('equipamentos.xlsx', 'analise.xlsx').process()
